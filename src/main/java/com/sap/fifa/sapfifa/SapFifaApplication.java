ef6c7dfea2e742e920edda72c7e3f26a08c05b57
Ronei Berlezi <r.berlezi@sap.com>
2019-01-08 10:29:55 -0200

    initial commit

---
+++ src/main/java/com/sap/fifa/sapfifa/SapFifaApplication.java
@@ -0,0 +1,16 @@
+package com.sap.fifa.sapfifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapFifaApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SapFifaApplication.class, args);
+    }
+
+}
+
